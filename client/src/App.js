1a4aea5bd2dd7cdc154213742db11dd011897a5b
Victor Isidoro <31276085+vtr84@users.noreply.github.com>
2021-09-15 19:13:46 +0200

    Tailwind and fonts test passed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      Community
-    </div>
+    <Router>
+      <Switch>
+      <>
+        <div className="flex justify-around w-full shadow-xl py-2 absolute fixed">
+          <Link className="py-2 px-2 text-mono font-bold bg-flime transition-colors ease-in-out duration-500 hover:bg-fblue text-xs text-black hover:text-white" to="/join-our-community">Join Our Community</Link>
+          <Link className="py-2 px-2 text-mono font-bold bg-fblue transition-colors ease-in-out duration-500 hover:bg-flime text-xs text-white hover:text-black" to="/social">Sign In</Link>
+        </div>
+        <div className="flex h-screen justify-center items-center w-full overflow-auto">
+          <p className="text-2xl">hey</p>
+        </div>
+      </>
+      </Switch>
+    </Router>
   );
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,30 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fblue: {
+          light: '#4A8CE1',
+          DEFAULT: '#0063e2',
+          dark: '#00449C',
+        },
+        fred: {
+          light: '#F46453',
+          DEFAULT: '#f6331c',
+          dark: '#C81702',
+        },
+        fpink: {
+          light: '#FDBAD3',
+          DEFAULT: '#ee93b5',
+          dark: '#D31059',
+        },
+        flime: {
+          light: '#E3F869',
+          DEFAULT: '#d7fb03',
+          dark: '#A4BF06',
+        }
+      }
+    }
   },
   variants: {
     extend: {},
