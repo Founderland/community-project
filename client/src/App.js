dea54dbf7f609956b0b0f235566f69fa62419463
Victor Isidoro <31276085+vtr84@users.noreply.github.com>
2021-09-19 17:14:52 +0200

    cleaned up route

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import Applicants from './components/Applicants';
 
 function App() {
   return (
     <Router>
       <Switch>
-      <>
-        <div className="flex justify-around w-full shadow-xl py-2 absolute fixed">
-          <Link className="py-2 px-2 text-mono font-bold bg-flime transition-colors ease-in-out duration-500 hover:bg-fblue text-xs text-black hover:text-white" to="/join-our-community">Join Our Community</Link>
-          <Link className="py-2 px-2 text-mono font-bold bg-fblue transition-colors ease-in-out duration-500 hover:bg-flime text-xs text-white hover:text-black" to="/social">Sign In</Link>
-        </div>
-        <div className="flex h-screen justify-center items-center w-full overflow-auto">
-          <p className="text-2xl">hey</p>
-        </div>
-      </>
+        <Route exact path="/">
+          <div className="flex flex-start w-full shadow-xl py-2 absolute fixed">
+            <Link className="py-2 px-2 text-mono font-bold bg-flime transition-colors ease-in-out duration-500 hover:bg-fblue text-xs text-black hover:text-white" to="/join-our-community">Join Our Community</Link>
+            <Link className="py-2 px-2 text-mono font-bold bg-fblue transition-colors ease-in-out duration-500 hover:bg-flime text-xs text-white hover:text-black" to="/social">Sign In</Link>
+          </div>
+        </Route>
+        <Route exact path="/social">
+          <div className="flex h-screen justify-center items-center w-full overflow-auto">
+            <p className="text-2xl">hey</p>
+          </div>
+          </Route>
+        <Route exact path="/join-our-community">
+          <Applicants />
+        </Route>
       </Switch>
     </Router>
   );
