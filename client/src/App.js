8ea5d8e61b2743932d6902415145a083795540b6
Victor Isidoro <31276085+vtr84@users.noreply.github.com>
2021-09-19 18:43:45 +0200

    Basic login design

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
-import Applicants from './components/Applicants';
+import Applicants from './components/Applicants'
+import Admin from './components/Admin'
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path="/admin">
+          <Admin />
+        </Route>
         <Route exact path="/">
           <div className="flex flex-start w-full shadow-xl py-2 absolute fixed">
             <Link className="py-2 px-2 text-mono font-bold bg-flime transition-colors ease-in-out duration-500 hover:bg-fblue text-xs text-black hover:text-white" to="/join-our-community">Join Our Community</Link>
