0531f017995f211e504bf80765f3f625238d7146
Salvo <salvopatti110@gmail.com>
2021-09-22 12:29:25 +0200

    basic form db structure and front-end layout

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,45 +1,145 @@
 import StepWizard from "react-step-wizard";
+import axios from "axios";
+
 const dumyData = [
   {
-    question: "blabla",
+    _id: "614af929a47c96b7a3decf1e",
+    category: "About You",
+    question: "Name",
+    rank: "Not Important - just for info/further context",
     type: "open",
-    category: "about_you",
+    answers: [
+      {
+        answer: "open",
+        ideal: "yes",
+        points: 0,
+        notes: "",
+        _id: "614af929a47c96b7a3decf1f",
+      },
+    ],
+    __v: 0,
   },
   {
-    question: "blabla",
-    type: "multiple",
-    answers: ["yes", "no", "other"],
-    category: "tell_us_more",
-  },
-  {
-    question: "blabla",
+    _id: "614af997a47c96b7a3decf21",
+    category: "About You",
+    question: "Title/Position",
+    rank: "Not Important - just for info/further context",
     type: "open",
-    category: "tell_us_more",
+    answers: [
+      {
+        answer: "open",
+        ideal: "yes",
+        points: 0,
+        notes: "",
+        _id: "614af997a47c96b7a3decf22",
+      },
+    ],
+    __v: 0,
   },
   {
-    question: "blabla",
+    _id: "614af9aaa47c96b7a3decf24",
+    category: "About You",
+    question: "Contact Information",
+    rank: "Not Important - just for info/further context",
     type: "open",
-    category: "about_you",
+    answers: [
+      {
+        answer: "open",
+        ideal: "yes",
+        points: 0,
+        notes: "",
+        _id: "614af9aaa47c96b7a3decf25",
+      },
+    ],
+    __v: 0,
   },
   {
-    question: "blabla",
-    type: "open",
-    category: "about_your_business",
+    _id: "614afa73a47c96b7a3decf27",
+    category: "About You",
+    question:
+      "Do you identify as a woman who has faced obstacles tied to your ethnicity/race and gender in your entrepreneurial journey?",
+    rank: "Very Important - variable is scrutinized",
+    type: "choice",
+    answers: [
+      {
+        answer: "Yes",
+        ideal: "yes",
+        points: 50,
+        notes: "",
+        _id: "614afa73a47c96b7a3decf28",
+      },
+      {
+        answer: "No",
+        ideal: "yes",
+        points: 0,
+        notes: "",
+        _id: "614afa73a47c96b7a3decf29",
+      },
+      {
+        answer: "open",
+        ideal: "yes",
+        points: 0,
+        notes: "",
+        _id: "614afa73a47c96b7a3decf2a",
+      },
+    ],
+    __v: 0,
   },
   {
-    question: "blabla",
-    type: "multiple",
-    answers: ["yes", "no", "maybe"],
-    category: "about_your_business",
+    _id: "614afc27a47c96b7a3decf2c",
+    category: "About Your Business",
+    question: "Where is your Business registered (based)?",
+    rank: "Vital - Deal Maker or Breaker",
+    type: "choice",
+    answers: [
+      {
+        answer: "Continental Europe",
+        ideal: "yes",
+        points: 50,
+        notes: "",
+        _id: "614afc27a47c96b7a3decf2d",
+      },
+      {
+        answer: "United Kingdom",
+        ideal: "yes",
+        points: 50,
+        notes: "",
+        _id: "614afc27a47c96b7a3decf2e",
+      },
+      {
+        answer:
+          "Not in Europe or the UK but planning on relocating to that area",
+        ideal: "yes",
+        points: 200,
+        notes: "",
+        _id: "614afc27a47c96b7a3decf2f",
+      },
+      {
+        answer: "Not in Europe or the UK",
+        ideal: "no",
+        points: 0,
+        notes: "",
+        _id: "614afc27a47c96b7a3decf30",
+      },
+    ],
+    __v: 0,
   },
 ];
 
-const Question = ({ question, type, category, answers }) => {
+const Question = ({ _id, question, type, category, answers }) => {
+  console.log(answers);
   return (
     <div>
       <label>{question}</label>
       <br />
-      <label> </label>
+      <Answers type={type} answers={answers} />
+    </div>
+  );
+};
+
+const Answers = ({ type, answers }) => {
+  return (
+    <div>
       {type === "open" ? (
         <input
           type="text"
@@ -54,18 +154,23 @@ const Question = ({ question, type, category, answers }) => {
             <>
               <input
                 type="radio"
-                id="html"
+                id={answer._id}
                 name="fav_language"
-                value={answer}
+                value={answer.answer}
               />
-              <label for="html"> {answer}</label>
-              {answer !== "other" && <br />}
-              {answer === "other" && (
+              {answer.answer !== "open" && (
+                <>
+                  <label for="html"> {answer.answer}</label>
+                  <br />
+                </>
+              )}
+              {answer.answer === "open" && (
                 <input
                   type="text"
+                  id={answer._id}
                   className="form-control"
-                  name={answer}
-                  placeholder="First Name"
+                  name={answer.answer}
+                  placeholder="open"
                   // onChange={update}
                 />
               )}
@@ -83,14 +188,13 @@ const First = (props) => {
   // };
 
   const { questions } = props;
-  console.log(props);
   return (
     <div className="h-full w-full flex justify-center flex-col">
       <h3 className="text-center ">Welcome to {questions[0].category}</h3>
       <h2>Current Step {props.currentStep}</h2>
       <div className="bg-gray-200 p-5">
-        {questions.map((question) => (
-          <Question {...question} />
+        {questions.map((question, i) => (
+          <Question key={i} {...question} />
         ))}
       </div>
       {/* <Stats step={1} {...props} /> */}
@@ -109,7 +213,6 @@ const Second = (props) => {
   // };
 
   const { questions } = props;
-  console.log(props);
 
   return (
     <div>
@@ -117,8 +220,8 @@ const Second = (props) => {
         <h3 className="text-center ">Welcome to {questions[0].category}</h3>
         <h2>Current Step {props.currentStep}</h2>
         <div className="bg-gray-200 p-5">
-          {questions.map((question) => (
-            <Question {...question} />
+          {questions.map((question, i) => (
+            <Question key={i} {...question} />
           ))}
         </div>
         {/* <Stats step={1} {...props} /> */}
@@ -155,19 +258,16 @@ const Form = ({ match }) => {
   // const { memberType } = match.params;
 
   const aboutYouQuestions = dumyData.filter(
-    (item) => item.category === "about_you"
-  );
-  const tellUsMore = dumyData.filter(
-    (item) => item.category === "tell_us_more"
+    (item) => item.category === "About You"
   );
   const aboutYourBusiness = dumyData.filter(
-    (item) => item.category === "about_your_business"
+    (item) => item.category === "About Your Business"
   );
 
   return (
     <StepWizard isHashEnabled>
-      <First hashKey={"FirstStep"} questions={tellUsMore} />
-      <Second hashKey={"SecondStep"} questions={aboutYouQuestions} />
+      <First hashKey={"FirstStep"} questions={aboutYouQuestions} />
+      <Second hashKey={"SecondStep"} questions={aboutYourBusiness} />
       <Third questions={aboutYourBusiness} />
     </StepWizard>
   );
