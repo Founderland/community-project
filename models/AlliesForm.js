8c3476b1c5b62b066cb259c21e0e8c908b29f941
Salvo <salvopatti110@gmail.com>
2021-09-29 02:32:58 +0200

    added Allies schema, controller and routes

---
+++ models/AlliesForm.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+// const Investor = require("./InvestorApplicant");
+
+const alliesFormSchema = new mongoose.Schema({
+    category: { type: String, required: true },
+    question: { type: String, required: true },
+    type: {
+        type: String,
+        enum: ['open', 'list', 'choice', 'multiple'],
+        required: true,
+    },
+    answers: [
+        {
+            answer: { type: String, required: true },
+        },
+    ],
+    categoryPage: { type: Number, required: true },
+})
+
+const AlliesForm = mongoose.model('AlliesForm', alliesFormSchema)
+
+module.exports = AlliesForm
