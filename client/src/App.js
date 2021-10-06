68296c3750b0522af2e2feedcf53a264a1fe5176
Victor Isidoro <victorisidoro@icloud.com>
2021-10-06 21:19:17 +0100

    Homepage component

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,25 @@ import ApplicantsDispatcher from './components/ApplicantsDispatcher'
 import Form from './components/Form'
 import Thankyou from './components/Forms/Thankyou'
 import AnswersProvider from './contexts/AnswersProvider'
+import Homepage from './components/Homepage.js'
 import Admin from './components/Admin/Admin'
 
 function App() {
-    return (
-        <AnswersProvider>
-            <Router>
-                <Route
-                    exact
-                    path="/join-our-community"
-                    component={ApplicantsDispatcher}
-                />
-                <Route exact path="/form/:memberType" component={Form} />
-                <Route exact path="/thankyou" component={Thankyou} />
-                <Route exact path="/admin" component={Admin} />
-            </Router>
-        </AnswersProvider>
-    )
+  return (
+    <AnswersProvider>
+      <Router>
+        <Route exact path="/" component={Homepage} />
+        <Route
+          exact
+          path="/join-our-community"
+          component={ApplicantsDispatcher}
+        />
+        <Route exact path="/form/:memberType" component={Form} />
+        <Route exact path="/thankyou" component={Thankyou} />
+        <Route exact path="/admin" component={Admin} />
+      </Router>
+    </AnswersProvider>
+  )
 }
 
 export default App
