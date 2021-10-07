1a93dc8645125dd840fe7f1112fa07e55a97dae1
Sashmu <sasmithanagesh@gmail.com>
2021-10-07 15:53:16 +0200

    form validation

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,34 +1,35 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        fblue: {
-          light: '#4A8CE1',
-          DEFAULT: '#0063e2',
-          dark: '#00449C',
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                fblue: {
+                    light: '#4A8CE1',
+                    DEFAULT: '#0063e2',
+                    dark: '#00449C',
+                },
+                fred: {
+                    light: '#F46453',
+                    DEFAULT: '#f6331c',
+                    dark: '#C81702',
+                },
+                fpink: {
+                    light: '#FDBAD3',
+                    DEFAULT: '#ee93b5',
+                    dark: '#D31059',
+                },
+                flime: {
+                    light: '#E3F869',
+                    DEFAULT: '#d7fb03',
+                    dark: '#A4BF06',
+                },
+            },
         },
-        fred: {
-          light: '#F46453',
-          DEFAULT: '#f6331c',
-          dark: '#C81702',
-        },
-        fpink: {
-          light: '#FDBAD3',
-          DEFAULT: '#ee93b5',
-          dark: '#D31059',
-        },
-        flime: {
-          light: '#E3F869',
-          DEFAULT: '#d7fb03',
-          dark: '#A4BF06',
-        }
-      }
-    }
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
