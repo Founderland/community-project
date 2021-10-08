02485d3207664286dbc71d37549b7a89ed782b57
Victor Isidoro <victorisidoro@icloud.com>
2021-10-09 00:33:34 +0100

    Notifications component with context

--- client/src/components/Admin/Notifications.js
+++ client/src/components/Admin/Notifications.js
@@ -8,12 +8,24 @@ import {
 import AdminContext from "../../contexts/Admin"
 
 const Notifications = () => {
+  let bell = false
   const { notifications } = useContext(AdminContext)
+  if (notifications.length) {
+    bell = true
+  }
   return (
     <Menu as="div" className="relative outline-none">
-      <Menu.Button className="flex text-gray-600 focus:outline-none">
-        <BellIcon className="h-7 w-7 sm:w-5 sm:h-5" />
-        <div className="h-1 w-1 p-1 bg-fred rounded-full sm:-mx-2 -mx-3 animate-pulse"></div>
+      <Menu.Button
+        className={`flex ${
+          bell ? "text-gray-800" : "text-gray-300"
+        } focus:outline-none`}
+      >
+        <BellIcon className="h-7 w-7 sm:w-5 sm:h-5 " />
+        <div
+          className={`h-1 w-1 p-1 animate-pulse rounded-full sm:-mx-2 -mx-3 ${
+            bell ? "bg-fred" : "bg-transparent"
+          }`}
+        ></div>
       </Menu.Button>
       <Transition
         as={Fragment}
@@ -25,22 +37,29 @@ const Notifications = () => {
         leaveTo="transform opacity-0 scale-95"
       >
         <Menu.Items className="absolute outline-none right-0 mt-2 bg-white w-80 overflow-hidden shadow-xl focus:outline-none">
-          {notifications.lenght
-            ? notifications.map((item) => (
-                <Menu.Item
-                  as="button"
-                  key={item.text}
-                  className="flex items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-fblue hover:text-white outline-none"
-                >
-                  {item.icon === "user" ? (
-                    <UserGroupIcon className="w-4 mr-2" />
-                  ) : (
-                    <ClipboardCheckIcon className="w-4 mr-2" />
-                  )}
-                  <p>{item.text}</p>
-                </Menu.Item>
-              ))
-            : "No "}
+          {notifications.length ? (
+            notifications.map((item) => (
+              <Menu.Item
+                as="button"
+                key={item.text}
+                className="flex items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-fblue hover:text-white outline-none"
+              >
+                {item.icon === "user" ? (
+                  <UserGroupIcon className="w-4 mr-2" />
+                ) : (
+                  <ClipboardCheckIcon className="w-4 mr-2" />
+                )}
+                <p>{item.text}</p>
+              </Menu.Item>
+            ))
+          ) : (
+            <Menu.Item
+              as="button"
+              className="flex items-center w-full px-4 py-2 text-sm text-gray-700 hover:bg-fblue hover:text-white outline-none"
+            >
+              <p>No new notifications</p>
+            </Menu.Item>
+          )}
         </Menu.Items>
       </Transition>
     </Menu>
