903b91cbd2f97672c76479764dd5c7893874870b
Victor Isidoro <31276085+vtr84@users.noreply.github.com>
2021-10-17 10:31:35 +0100

    test for ScrollToTop on content render

--- client/src/components/Admin/Content.js
+++ client/src/components/Admin/Content.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef } from "react";
 import AdminContext from "../../contexts/Admin";
 import QuestionsList from "./QuestionsList";
 import Dashboard from "./Dashboard";
@@ -8,36 +8,38 @@ import ResponseList from "./ResponseList";
 import Members from "./Members";
 
 const Content = () => {
-     const { view, views, setMemberType, setApplicantType } =
-          useContext(AdminContext);
+    const { view, views, setMemberType, setApplicantType } =
+        useContext(AdminContext);
+    //SCROLL BACK UP ON MENU CHANGE
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [view]);
 
-     useEffect(() => {
-          if (views[view].name.includes("Form")) {
-               setMemberType(views[view].name.toLowerCase().split(" ")[0]);
-          }
-          if (views[view].name.includes("Applicants")) {
-               setApplicantType(views[view].name.split(" ")[0]);
-          }
-     }, [view, views, setApplicantType, setMemberType]);
+    useEffect(() => {
+        if (views[view].name.includes("Form")) {
+            setMemberType(views[view].name.toLowerCase().split(" ")[0]);
+        }
+        if (views[view].name.includes("Applicants")) {
+            setApplicantType(views[view].name.split(" ")[0]);
+        }
+    }, [view, views, setApplicantType, setMemberType]);
 
-     return (
-          <main className="overflow-x-hidden">
-               <div className="items-center mx-auto md:px-4 py-2">
-                    <div className="flex justify-center h-screen">
-                         {views[view].name === "Dashboard" && <Dashboard />}
-                         {views[view].name === "Members" && <Members />}
-                         {views[view].name.includes("Form") && (
-                              <QuestionsList />
-                         )}
-                         {views[view].name.includes("Applicants") && (
-                              <ResponseList />
-                         )}
-                         {views[view].name === "Profile" && <Profile />}
-                         {views[view].name === "Settings" && <Settings />}
-                    </div>
-               </div>
-          </main>
-     );
+    return (
+        <main className="overflow-x-hidden">
+            <div className="items-center mx-auto md:px-4 py-2">
+                <div className="flex justify-center h-screen">
+                    {views[view].name === "Dashboard" && <Dashboard />}
+                    {views[view].name === "Members" && <Members />}
+                    {views[view].name.includes("Form") && <QuestionsList />}
+                    {views[view].name.includes("Applicants") && (
+                        <ResponseList />
+                    )}
+                    {views[view].name === "Profile" && <Profile />}
+                    {views[view].name === "Settings" && <Settings />}
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default Content;
