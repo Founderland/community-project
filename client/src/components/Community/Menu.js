125a9d32602a878532203b006e5952b914fa9848
Victor Isidoro <31276085+vtr84@users.noreply.github.com>
2021-10-17 16:02:41 +0100

    sign up token processed

--- client/src/components/Community/Menu.js
+++ client/src/components/Community/Menu.js
@@ -1,63 +1,63 @@
 import {
-  NewspaperIcon,
-  CalendarIcon,
-  ChatAlt2Icon,
-  CollectionIcon,
-  UserGroupIcon,
-} from "@heroicons/react/outline"
-import { useContext } from "react"
-import UserContext from "../../contexts/User"
+    NewspaperIcon,
+    CalendarIcon,
+    ChatAlt2Icon,
+    CollectionIcon,
+    UserGroupIcon
+} from "@heroicons/react/outline";
+import { useContext } from "react";
+import UserContext from "../../contexts/User";
 
 const Menu = () => {
-  const { view, setView } = useContext(UserContext)
-  return (
-    <div className="fixed bottom-0 w-full">
-      <div className="md:hidden bottom-0 w-full bg-fblue text-xs">
-        <div className="flex text-white text-center">
-          <button
-            onClick={() => setView(0)}
-            className={`flex justify-center p-4 hover:bg-fpink hover:text-black w-1/5 justify-center ${
-              view === 0 ? "bg-fpink text-black" : ""
-            }`}
-          >
-            <NewspaperIcon className="h-6" />
-          </button>
-          <button
-            onClick={() => setView(1)}
-            className={`flex justify-center p-4 hover:bg-fpink hover:text-black w-1/5 justify-center ${
-              view === 1 ? "bg-fpink text-black" : ""
-            }`}
-          >
-            <UserGroupIcon className="h-6" />
-          </button>
-          <button
-            onClick={() => setView(2)}
-            className={`flex justify-center p-4 hover:bg-fpink hover:text-black w-1/5 justify-center ${
-              view === 2 ? "bg-fpink text-black" : ""
-            }`}
-          >
-            <CalendarIcon className="h-6" />
-          </button>
-          <button
-            onClick={() => setView(3)}
-            className={`flex justify-center p-4 hover:bg-fpink hover:text-black w-1/5 justify-center ${
-              view === 3 ? "bg-fpink text-black" : ""
-            }`}
-          >
-            <CollectionIcon className="h-6" />
-          </button>
-          <button
-            onClick={() => setView(4)}
-            className={`flex justify-center p-4 hover:bg-fpink hover:text-black w-1/5 justify-center ${
-              view === 4 ? "bg-fpink text-black" : ""
-            }`}
-          >
-            <ChatAlt2Icon className="h-6" />
-          </button>
+    const { view, setView } = useContext(UserContext);
+    return (
+        <div className="fixed bottom-0 w-full">
+            <div className="md:hidden bottom-0 w-full bg-black text-xs">
+                <div className="flex text-white text-center">
+                    <button
+                        onClick={() => setView(0)}
+                        className={`flex justify-center p-4 hover:bg-fpink hover:text-black w-1/5 justify-center ${
+                            view === 0 ? "bg-fpink text-black" : ""
+                        }`}
+                    >
+                        <NewspaperIcon className="h-6" />
+                    </button>
+                    <button
+                        onClick={() => setView(1)}
+                        className={`flex justify-center p-4 hover:bg-fpink hover:text-black w-1/5 justify-center ${
+                            view === 1 ? "bg-fpink text-black" : ""
+                        }`}
+                    >
+                        <UserGroupIcon className="h-6" />
+                    </button>
+                    <button
+                        onClick={() => setView(2)}
+                        className={`flex justify-center p-4 hover:bg-fpink hover:text-black w-1/5 justify-center ${
+                            view === 2 ? "bg-fpink text-black" : ""
+                        }`}
+                    >
+                        <CalendarIcon className="h-6" />
+                    </button>
+                    <button
+                        onClick={() => setView(3)}
+                        className={`flex justify-center p-4 hover:bg-fpink hover:text-black w-1/5 justify-center ${
+                            view === 3 ? "bg-fpink text-black" : ""
+                        }`}
+                    >
+                        <CollectionIcon className="h-6" />
+                    </button>
+                    <button
+                        onClick={() => setView(4)}
+                        className={`flex justify-center p-4 hover:bg-fpink hover:text-black w-1/5 justify-center ${
+                            view === 4 ? "bg-fpink text-black" : ""
+                        }`}
+                    >
+                        <ChatAlt2Icon className="h-6" />
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Menu
+export default Menu;
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,88 +1,91 @@
-require("dotenv").config()
-const User = require("../models/User")
-const Member = require("../models/Member")
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
+require("dotenv").config();
+const User = require("../models/User");
+const Member = require("../models/Member");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const authenticateUser = async (username, password, done) => {
-  //NEEDS TO BE ADAPTED TO CHECK BOTH ON COMMUNITY AND ADMIN
-  try {
-    // Check that user exists by email
-    const user = await User.findOne({ email: username })
-    if (user && (await bcrypt.compare(password, user.hashedPassword))) {
-      delete user.hashedPassword
-      done(null, user, { message: "Successful" })
-    } else {
-      user = await Member.findOne({ email: username })
-      if (user && (await bcrypt.compare(password, user.hashedPassword))) {
-        delete user.hashedPassword
-        done(null, user, { message: "Successful" })
-      } else {
-        done(null, false, { message: "Wrong Credentials" })
-      }
+    //NEEDS TO BE ADAPTED TO CHECK BOTH ON COMMUNITY AND ADMIN
+    try {
+        // Check that user exists by email
+        const user = await User.findOne({ email: username });
+        console.log("hey");
+        if (user && (await bcrypt.compare(password, user.hashedPassword))) {
+            delete user.hashedPassword;
+            done(null, user, { message: "Successful" });
+        } else {
+            user = await Member.findOne({ email: username });
+            if (user && (await bcrypt.compare(password, user.hashedPassword))) {
+                delete user.hashedPassword;
+                done(null, user, { message: "Successful" });
+            } else {
+                done(null, false, { message: "Wrong Credentials" });
+            }
+        }
+    } catch (err) {
+        done(null, false, { message: "Database Error" });
     }
-  } catch (err) {
-    done(null, false, { message: "Database Error" })
-  }
-}
+};
 
 const isAuthorized = async (payload, done) => {
-  //CHECK TOKEN FROM BOTH COMMUNITY AND ADMIN
-  User.findOne({ id: payload.id, email: payload.email }, (err, user) => {
-    if (err) {
-      return done(err, false)
-    }
-    if (user) {
-      return done(null, user)
-    } else {
-      Member.findOne({ id: payload.id, email: payload.email }, (err, user) => {
-        console.log(err)
+    //CHECK TOKEN FROM BOTH COMMUNITY AND ADMIN
+    User.findOne({ _id: payload.id, email: payload.email }, (err, user) => {
         if (err) {
-          return done(err, false)
+            return done(err, false);
         }
         if (user) {
-          return done(null, user)
+            return done(null, user);
         } else {
-          return done(null, false)
+            Member.findOne(
+                { _id: payload.id, email: payload.email },
+                (err, user) => {
+                    if (err) {
+                        return done(err, false);
+                    }
+                    if (user) {
+                        return done(null, user);
+                    } else {
+                        return done(null, false);
+                    }
+                }
+            );
         }
-      })
-    }
-  })
-}
+    });
+};
 
 const authorizeUser = async (req, res) => {
-  //CAN BE USED TO PROVIDED TOKENS BOTH TO COMMUNITY AND ADMIN - BUT PAYLOAD MIGHT BE DIFFERENT
-  if (req.user.id) {
-    let payload = {}
-    //MAKE A DIFFERENTE PAYLOAD FOR COMMUNITY
-    if (req.user.avatar) {
-      payload = {
-        id: req.user._id,
-        firstName: req.user.firstName,
-        lastName: req.user.lastName,
-        avatar: req.user.avatar,
-        role: req.user.role,
-        email: req.user.email,
-      }
+    //CAN BE USED TO PROVIDED TOKENS BOTH TO COMMUNITY AND ADMIN - BUT PAYLOAD MIGHT BE DIFFERENT
+    if (req.user.id) {
+        let payload = {};
+        //MAKE A DIFFERENTE PAYLOAD FOR COMMUNITY
+        if (req.user.avatar) {
+            payload = {
+                id: req.user._id,
+                firstName: req.user.firstName,
+                lastName: req.user.lastName,
+                avatar: req.user.avatar,
+                role: req.user.role,
+                email: req.user.email
+            };
+        } else {
+            payload = {
+                id: req.user._id,
+                firstName: req.user.firstName,
+                lastName: req.user.lastName,
+                email: req.user.email
+            };
+        }
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRE
+        });
+        return res.status(200).json({
+            access_token: token,
+            token_type: "bearer",
+            expires: process.env.JWT_EXPIRE
+        });
     } else {
-      payload = {
-        id: req.user._id,
-        firstName: req.user.firstName,
-        lastName: req.user.lastName,
-        email: req.user.email,
-      }
+        res.status(401).json({ message: "wrong credentials" });
     }
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRE,
-    })
-    return res.status(200).json({
-      access_token: token,
-      token_type: "bearer",
-      expires: process.env.JWT_EXPIRE,
-    })
-  } else {
-    res.status(401).json({ message: "wrong credentials" })
-  }
-}
+};
 
-module.exports = { authenticateUser, isAuthorized, authorizeUser }
+module.exports = { authenticateUser, isAuthorized, authorizeUser };
