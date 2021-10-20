9bcea6bc244d6a703c4b2b774615fefb9bd5170b
Victor Isidoro <victorisidoro@icloud.com>
2021-10-20 15:18:53 +0100

    add question redesign with admin language design

--- client/src/components/Admin/Forms/Question.js
+++ client/src/components/Admin/Forms/Question.js
@@ -197,23 +197,20 @@ const Question = ({ role }) => {
       className="relative py-1 bg-white w-full lg:w-5/6 px-4 mx-auto mt-6 flex justify-center"
     >
       <SubmitResponse result={result} />
-      <div className="w-full flex flex-col justify-center items-center bg-white p-4  shadow ">
-        <h1 className="font-bold p-3">
+      <div className="w-full flex flex-col justify-center items-center bg-white p-4 shadow-md ">
+        <h1 className="font-bold p-3 text-xl text-mono">
           {id !== "new" ? "Edit" : "Add new"} Question
         </h1>
         <div className=" w-full md:px-5">
-          <div className=" py-5 flex flex-col items-between justify-between lg:flex-row xl:justify-start lg:items-center ">
-            <label
-              HtmlFor="newQuestion"
-              className=" w-full  font-bold lg:w-1/6  mb-5 lg:m-0 "
-            >
+          <div className="w-full mt-4 mb-3 px-2">
+            <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
               Question
             </label>
             <textarea
               type="text"
               id="newQuestion"
               name="newQuestion"
-              className="p-3 align-middle outline-none resize-none shadow-md w-full lg:w-5/6   "
+              className="appearance-none outline-none block w-full bg-grey-lighter text-grey-darker border py-3 px-4 mb-3 shadow-md"
               placeholder="New question"
               value={questionInfo.question}
               onChange={(e) => {
@@ -225,18 +222,15 @@ const Question = ({ role }) => {
               }}
             />
           </div>
-          <div className="flex flex-col items-start lg:flex-wrap lg:flex-row lg:justify-start lg:justify-between lg:items-center ">
-            <div className="w-full flex justify-between xl:justify-start xl:w-full">
-              <div className=" w-2/3 lg:w-1/2 xl:w-4/5  py-5  flex flex-col items-between justify-between lg:flex-row lg:justify-between lg:items-center xl:justify-start">
-                <label
-                  HtmlFor="newQuestion"
-                  className=" w-full font-bold lg:w-1/3 xl:w-1/4 mb-5  lg:mb-0"
-                >
-                  Category
-                </label>
+          <div className="md:flex w-full px-3">
+            <div className="w-full md:w-3/4 mb-3 px-2">
+              <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
+                Category
+              </label>
+              <div className="w-full">
                 <ListOption
                   options={categories}
-                  format={"lg:w-2/3 xl:w-3/4"}
+                  format={"w-full"}
                   choice={questionInfo.category}
                   setChoice={(value) => {
                     setQuestionInfo({
@@ -246,96 +240,94 @@ const Question = ({ role }) => {
                   }}
                 />
               </div>
-              <div className="  w-1/4 lg:w-1/4 xl:w-1/3  py-5  flex flex-col items-between justify-between lg:flex-row xl:justify-center lg:items-center">
+            </div>
+            <div className=" w-full md:w-1/4 mb-3 px-2">
+              <label
+                HtmlFor="newQuestion"
+                className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+              >
+                Category Page
+              </label>
+              <ListOption
+                options={[
+                  { name: "1", value: 1 },
+                  { name: "2", value: 2 },
+                  { name: "3", value: 3 },
+                  { name: "4", value: 4 },
+                  { name: "5", value: 5 },
+                ]}
+                format={"w-full"}
+                choice={questionInfo.categoryPage}
+                setChoice={(value) =>
+                  setQuestionInfo({
+                    ...questionInfo,
+                    categoryPage: Number(value),
+                  })
+                }
+              />
+            </div>
+          </div>
+          <div className="md:flex w-full px-3">
+            {role === "founder" && (
+              <div className="w-full md:w-3/4 mb-3 px-2">
                 <label
-                  HtmlFor="newQuestion"
-                  className=" w-full  font-bold lg:w-3/4 xl:w-3/6 mb-5 lg:mb-0  xl:mx-2 lg:text-center"
+                  HtmlFor="rank"
+                  className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
                 >
-                  Category Page
+                  Rank
                 </label>
                 <ListOption
-                  options={[
-                    { name: "1", value: 1 },
-                    { name: "2", value: 2 },
-                    { name: "3", value: 3 },
-                    { name: "4", value: 4 },
-                    { name: "5", value: 5 },
-                  ]}
-                  format={"w-full"}
-                  choice={questionInfo.categoryPage}
-                  setChoice={(value) =>
-                    setQuestionInfo({
-                      ...questionInfo,
-                      categoryPage: Number(value),
-                    })
-                  }
-                />
-              </div>
-            </div>
-            <div className="w-full   py-5  flex flex-col items-between lg:flex-row lg:items-center justify-start ">
-              {/* Hiding Rank for non founder memebers */}
-              {role === "founder" && (
-                <>
-                  <label
-                    HtmlFor="rank"
-                    className="font-bold lg:w-1/6  mb-5 lg:mb-0  "
-                  >
-                    Rank
-                  </label>
-                  <ListOption
-                    options={ranks}
-                    format={" w-full lg:w-3/6 "}
-                    choice={questionInfo.rank}
-                    setChoice={(value) => {
-                      setQuestionInfo({
-                        ...questionInfo,
-                        rank: value,
-                      })
-                    }}
-                  />
-                </>
-              )}
-            </div>
-            <div className="w-full   py-5  flex flex-row  items-start lg:items-center justify-between lg:flex-row xl:justify-start ">
-              <div className="flex flex-col lg:flex-row lg:items-center xl:justify-start w-2/4">
-                <label
-                  HtmlFor="newQuestion"
-                  className="font-bold lg:w-1/3  mb-5   lg:mb-0 "
-                >
-                  Type of answer
-                </label>
-                <ListOption
-                  options={types}
-                  format={" w-full lg:w-2/3  "}
-                  choice={questionInfo.type}
+                  options={ranks}
+                  format={" w-full"}
+                  choice={questionInfo.rank}
                   setChoice={(value) => {
                     setQuestionInfo({
                       ...questionInfo,
-                      type: value,
+                      rank: value,
                     })
                   }}
                 />
               </div>
-              <div className="flex flex-col lg:flex-row justify-start md:justify-evenly xl:justify-center items-center w-2/4 ">
-                <label className=" font-bold text-center mb-9 lg:w-2/6 lg:mb-0 lg:ml-5">
-                  Mandatory{" "}
-                </label>
-                <input
-                  type="checkbox"
-                  className=" lg:w-1/6 w-5 h-5 "
-                  checked={questionInfo.mandatory}
-                  onChange={() =>
-                    setQuestionInfo({
-                      ...questionInfo,
-                      mandatory: !questionInfo.mandatory,
-                    })
-                  }
-                />
-              </div>
+            )}
+            <div className=" w-full md:w-1/4 mb-3 px-2">
+              <label
+                HtmlFor="newQuestion"
+                className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+              >
+                Type of answer
+              </label>
+              <ListOption
+                options={types}
+                format={" w-full "}
+                choice={questionInfo.type}
+                setChoice={(value) => {
+                  setQuestionInfo({
+                    ...questionInfo,
+                    type: value,
+                  })
+                }}
+              />
+            </div>
+            <div className=" w-full md:w-1/4 mb-3 px-2 flex items-center justify-center flex-col">
+              <label className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
+                Mandatory
+              </label>
+              <input
+                type="checkbox"
+                className=" lg:w-1/6 w-5 h-5 "
+                checked={questionInfo.mandatory}
+                onChange={() =>
+                  setQuestionInfo({
+                    ...questionInfo,
+                    mandatory: !questionInfo.mandatory,
+                  })
+                }
+              />
             </div>
           </div>
-          <div className=" py-5 flex flex-col flex-wrap  xl:flex-nowrap lg:flex-row items-center justify-between">
-            {questionInfo.type !== "open" && (
+
+          {questionInfo.type !== "open" && (
+            <div className=" py-5 flex flex-col flex-wrap  xl:flex-nowrap lg:flex-row items-center justify-between">
               <AnswerSection
                 answersList={answersList}
                 setAnswersList={setAnswersList}
@@ -344,12 +336,12 @@ const Question = ({ role }) => {
                 handleNewAnswer={handleNewAnswer}
                 memberType={role}
               />
-            )}
-          </div>
-          <div className=" flex flex-row w-full flex-wrap items-center justify-around mt-5">
+            </div>
+          )}
+          <div className="px-4 py-2 flex flex-col-reverse sm:flex-row items-center justify-around">
             <button
               type="button"
-              className=" flex justify-center items-center w-1/2 md:w-1/4 xl:w-1/6 items-center font-bold  p-4 bg-flime text-fblue  transition-colors ease-in-out duration-500 hover:bg-fblue  hover:text-white  "
+              className="flex justify-center items-center px-8 py-2 w-full shadow-lg sm:w-1/5 bg-fblue text-white transition duration-200 hover:bg-flime hover:text-black mb-4"
               onClick={() => setShowPreview(true)}
             >
               Preview
@@ -357,16 +349,16 @@ const Question = ({ role }) => {
             </button>
             <button
               type="button"
-              className="p-4 w-1/2 md:w-1/4 xl:w-1/6 bg-fblue text-white font-bold   transition-colors ease-in-out duration-500 hover:bg-flime  hover:text-fblue "
+              className="flex justify-center items-center px-8 py-2 w-full shadow-lg sm:w-1/5 bg-flime transition duration-200 hover:bg-fblue hover:text-white mb-4"
               onClick={handleSubmit}
             >
-              Submit
+              Save
             </button>
             <button
               type="button"
               className={
                 id !== "new"
-                  ? `flex justify-center items-center p-4 w-1/2 md:w-1/4 xl:w-1/6 bg-black text-white font-bold  transition-colors ease-in-out duration-500 hover:bg-fred-dark mt-10 md:mt-0 `
+                  ? `flex justify-center items-center px-8 py-2 w-full shadow-lg sm:w-1/5 bg-gray-700 transition duration-200 hover:bg-fred text-white mb-4`
                   : "hidden"
               }
               onClick={handleDelete}
