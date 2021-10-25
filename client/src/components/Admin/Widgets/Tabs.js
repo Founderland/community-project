5c74cc77965bc535e500ad838d91f447a2fa9d34
Victor Isidoro <victorisidoro@icloud.com>
2021-10-25 23:42:45 +0100

    tabs mobile view refined

--- client/src/components/Admin/Widgets/Tabs.js
+++ client/src/components/Admin/Widgets/Tabs.js
@@ -15,14 +15,14 @@ const Tabs = ({ tabs, selectedTab, setSelectedTab, id }) => {
         {tabs.map((tab) =>
           user.role.includes(tab.restricted) ? (
             <div
-              className={`transition ease-in-out duration-200 w-32 px-1 py-0.5 text-${color} text-sm text-mono tracking-wide font-medium outline-none flex justify-center items-center ${
+              className={`transition ease-in-out duration-200 w-24 md:w-32 px-1 py-0.5 text-${color} text-sm text-mono tracking-wide font-medium outline-none flex justify-center items-center ${
                 selectedTab === tab.index
                   ? "font-bold bg-white shadow"
                   : "text-white hover:bg-white hover:bg-opacity-20"
               }`}
               onClick={() => (id ? null : setSelectedTab(tab.index))}
             >
-              <p>{tab.name}</p>
+              <p className="text-xs md:text-md">{tab.name}</p>
             </div>
           ) : (
             ""
