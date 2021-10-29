1bccb60ac2880a631a369ad529940420cd496c76
Victor Isidoro <victorisidoro@icloud.com>
2021-10-29 15:25:08 +0100

    Tags suggestions showing

--- client/src/components/Admin/Widgets/Tags.js
+++ client/src/components/Admin/Widgets/Tags.js
@@ -1,29 +1,67 @@
 import { useState } from "react"
 
 const Tags = ({ tags, setTags }) => {
+  const defaultSuggestions = [
+    "health",
+    "food",
+    "education",
+    "finance",
+    "sport",
+    "conference",
+    "fundraiser",
+    "masterclass",
+  ]
+
   const [tag, setTag] = useState("")
+  const [showSuggestions, setShowSuggestions] = useState(false)
+  const [suggestions, setSuggestions] = useState(defaultSuggestions)
   const regex = /^[\w-]+$/
-  const onPaste = (value) => {
-    const result = value
-      .replace(/[^\w,;\s]+/g, ",")
-      .replace(/\s+/g, "-")
-      .replace(/[,;]{2,}/g, ",")
-    return result.split(/,+/)
+  const addTag = (value) => {
+    setTags([...tags, value])
+    setTag("")
+  }
+  const handleInput = (value) => {
+    setTag(value)
+    console.log(value)
+    if (value) {
+      setShowSuggestions(true)
+      const list = defaultSuggestions.filter((tag) => tag.includes(value))
+      list.unshift(value)
+      setSuggestions(list)
+    } else {
+      setShowSuggestions(false)
+    }
+  }
+  const handleSelect = (value) => {
+    console.log(value)
   }
   return (
     <div className="flex">
       <div className="relative flex-grow-0">
         <input
-          className="appearance-none outline-none block  bg-grey-lighter text-grey-darker focus:ring-2 ring-fblue border border-grey-lighter rounded py-3 px-4 mb-3"
+          className=" appearance-none outline-none block  bg-grey-lighter text-grey-darker focus:ring-2 ring-fblue border border-grey-lighter rounded py-3 px-4 mb-3"
           type="text"
           onChange={(e) => {
-            setTag(e.target.value)
+            handleInput(e.target.value)
           }}
           placeholder="Add a tag"
           value={tag}
         />
-        <div className="absolute bg-white border px-2 py-1">
-          test autocomplete
+        <div
+          className={`absolute top-12  bg-white border px-1 ${
+            showSuggestions ? "" : "hidden"
+          }`}
+        >
+          {suggestions.map((tag) => (
+            <div
+              className="w-full p-1"
+              onClick={(e) => {
+                handleSelect(tag)
+              }}
+            >
+              <p className="p-1 text-sm">{tag}</p>
+            </div>
+          ))}
         </div>
       </div>
       <div className="flex-grow p-3 flex flex-wrap justify-start space-x-2">
