8fc6e3e4adce262367a457fbd061be869b7b724d
Victor Isidoro <victorisidoro@icloud.com>
2021-10-30 11:46:52 +0100

    Search by Tags

--- client/src/components/Admin/Events/EventsList.js
+++ client/src/components/Admin/Events/EventsList.js
@@ -3,6 +3,9 @@ import axios from "axios"
 import AdminContext from "../../../contexts/Admin"
 import Loading from "../Widgets/Loading"
 import EventCard from "./EventCard"
+import Pagination from "../Widgets/Pagination"
+import { SearchIcon } from "@heroicons/react/solid"
+import { EmojiSadIcon } from "@heroicons/react/outline"
 
 const eventsUrl = "/api/events/"
 
@@ -13,8 +16,8 @@ const EventsList = ({ state }) => {
       title: "Fundraiser",
       photo: null,
       description: "Fundraiser for Climate",
-      dateStart: "2021-11-02T10:00:00.000Z",
-      dateEnd: "2021-11-04T12:00:00.000Z",
+      dateStart: new Date("2021-11-02T10:00:00.000Z"),
+      dateEnd: new Date("2021-11-04T12:00:00.000Z"),
       address: "Geschwister-Scholl-Straße",
       city: "Berlin",
       geoLocation: {
@@ -37,8 +40,176 @@ const EventsList = ({ state }) => {
       title: "Conference",
       photo: null,
       description: "Conference on Food Shortage",
-      dateStart: "2021-11-02T10:00:00.000Z",
-      dateEnd: "2021-11-04T12:00:00.000Z",
+      dateStart: new Date("2021-11-02T10:00:00.000Z"),
+      dateEnd: new Date("2021-11-04T12:00:00.000Z"),
+      address: "",
+      city: "",
+      geoLocation: {
+        lat: 52.5205888,
+        lng: 13.3912322,
+      },
+      type: "online",
+      link: "https://www.techonthenet.com/js/string_match.php",
+      tags: ["#food", "#speaker", "#online"],
+      annouce: false,
+      zoom: 18,
+      location: "",
+      organizer: { _id: "1", firstName: "Sasmitha", lastName: "Kumar" },
+      interested: [],
+      going: [],
+      canceled: false,
+    },
+    {
+      _id: "3",
+      title: "Conference",
+      photo: null,
+      description: "Conference on Food Shortage",
+      dateStart: new Date("2021-11-04T10:00:00.000Z"),
+      dateEnd: new Date("2021-11-04T12:00:00.000Z"),
+      address: "",
+      city: "",
+      geoLocation: {
+        lat: 52.5205888,
+        lng: 13.3912322,
+      },
+      type: "online",
+      link: "https://www.techonthenet.com/js/string_match.php",
+      tags: ["#food", "#speaker", "#online"],
+      annouce: false,
+      zoom: 18,
+      location: "",
+      organizer: { _id: "1", firstName: "Sasmitha", lastName: "Kumar" },
+      interested: [],
+      going: [],
+      canceled: false,
+    },
+    {
+      _id: "4",
+      title: "Conference",
+      photo: null,
+      description: "Conference on Food Shortage",
+      dateStart: new Date("2021-11-06T10:00:00.000Z"),
+      dateEnd: new Date("2021-11-07T12:00:00.000Z"),
+      address: "",
+      city: "",
+      geoLocation: {
+        lat: 52.5205888,
+        lng: 13.3912322,
+      },
+      type: "online",
+      link: "https://www.techonthenet.com/js/string_match.php",
+      tags: ["#health", "#speaker", "#online"],
+      annouce: false,
+      zoom: 18,
+      location: "",
+      organizer: { _id: "1", firstName: "Sasmitha", lastName: "Kumar" },
+      interested: [],
+      going: [],
+      canceled: false,
+    },
+    {
+      _id: "5",
+      title: "Conference",
+      photo: null,
+      description: "Conference on Food Shortage",
+      dateStart: new Date("2021-11-07T10:00:00.000Z"),
+      dateEnd: new Date("2021-11-07T12:00:00.000Z"),
+      address: "",
+      city: "",
+      geoLocation: {
+        lat: 52.5205888,
+        lng: 13.3912322,
+      },
+      type: "online",
+      link: "https://www.techonthenet.com/js/string_match.php",
+      tags: ["#food", "#speaker", "#online"],
+      annouce: false,
+      zoom: 18,
+      location: "",
+      organizer: { _id: "1", firstName: "Sasmitha", lastName: "Kumar" },
+      interested: [],
+      going: [],
+      canceled: false,
+    },
+    {
+      _id: "6",
+      title: "Conference",
+      photo: null,
+      description: "Conference on Food Shortage",
+      dateStart: new Date("2021-11-06T10:00:00.000Z"),
+      dateEnd: new Date("2021-11-06T12:00:00.000Z"),
+      address: "",
+      city: "",
+      geoLocation: {
+        lat: 52.5205888,
+        lng: 13.3912322,
+      },
+      type: "online",
+      link: "https://www.techonthenet.com/js/string_match.php",
+      tags: ["#food", "#speaker", "#online"],
+      annouce: false,
+      zoom: 18,
+      location: "",
+      organizer: { _id: "1", firstName: "Sasmitha", lastName: "Kumar" },
+      interested: [],
+      going: [],
+      canceled: false,
+    },
+    {
+      _id: "7",
+      title: "Conference",
+      photo: null,
+      description: "Conference on Food Shortage",
+      dateStart: new Date("2021-11-08T10:00:00.000Z"),
+      dateEnd: new Date("2021-11-08T12:00:00.000Z"),
+      address: "",
+      city: "",
+      geoLocation: {
+        lat: 52.5205888,
+        lng: 13.3912322,
+      },
+      type: "online",
+      link: "https://www.techonthenet.com/js/string_match.php",
+      tags: ["#food", "#speaker", "#online"],
+      annouce: false,
+      zoom: 18,
+      location: "",
+      organizer: { _id: "1", firstName: "Sasmitha", lastName: "Kumar" },
+      interested: [],
+      going: [],
+      canceled: false,
+    },
+    {
+      _id: "8",
+      title: "Halloween",
+      photo: null,
+      description: "Conference on Food Shortage",
+      dateStart: new Date("2021-10-31T10:00:00.000Z"),
+      dateEnd: new Date("2021-11-01T12:00:00.000Z"),
+      address: "",
+      city: "",
+      geoLocation: {
+        lat: 52.5205888,
+        lng: 13.3912322,
+      },
+      type: "online",
+      link: "https://www.techonthenet.com/js/string_match.php",
+      tags: ["#food", "#speaker", "#online"],
+      annouce: false,
+      zoom: 18,
+      location: "",
+      organizer: { _id: "1", firstName: "Sasmitha", lastName: "Kumar" },
+      interested: [],
+      going: [],
+      canceled: false,
+    },
+    {
+      _id: "9",
+      title: "Conference",
+      photo: null,
+      description: "Conference on Food Shortage",
+      dateStart: new Date("2021-11-02T10:00:00.000Z"),
+      dateEnd: new Date("2021-11-04T12:00:00.000Z"),
       address: "",
       city: "",
       geoLocation: {
@@ -58,32 +229,113 @@ const EventsList = ({ state }) => {
     },
   ])
   const [loading, setLoading] = useState(true)
-  const { token, reload, selectedTab, config } = useContext(AdminContext)
-  console.log(data)
+  const [offset, setOffset] = useState(0)
+  const [dataToDisplay, setDataToDisplay] = useState([])
+  const [perPage] = useState(6)
+  const [pageCount, setPageCount] = useState(0)
+  const [tags, setTags] = useState([])
+  const [searchTags, setSearchTags] = useState([])
+  const { reload, selectedTab, config } = useContext(AdminContext)
 
   useEffect(() => {
     axios
       .get(eventsUrl + state, config)
       .then((res) => {
         if (res.data.data.length) setData(res.data.data)
+        //GET ALL AVAILABLE TAGS
+        const allTags = data
+          .map((item) => item.tags)
+          .flat(1)
+          .filter((item, i, self) => i === self.indexOf(item))
+          .sort((a, b) => a.substring(1).length - b.substring(1).length)
+        setTags(allTags)
         setLoading(false)
       })
       .catch((e) => {
         console.log(e)
       })
-  }, [reload, selectedTab])
+  }, [reload, selectedTab, searchTags])
+
+  useEffect(() => {
+    let filteredData = [...data]
+    //FILTER BY TAGS
+    if (searchTags.length) {
+      searchTags.forEach(
+        (tag) =>
+          (filteredData = [
+            ...filteredData.filter((item) => item.tags.includes(tag)),
+          ])
+      )
+    }
+    //SORT DATA BY DATES
+    data.sort((a, b) => {
+      return a.dateStart - b.dateStart
+    })
+    const slice = filteredData.slice(
+      offset * perPage,
+      offset * perPage + perPage
+    )
+    setDataToDisplay(slice)
+    setPageCount(Math.ceil(data.length / perPage))
+
+    return () => {
+      setDataToDisplay([])
+    }
+  }, [data, offset, searchTags])
+
+  const filterTag = (value) => {
+    let newFilter = [...searchTags]
+    if (newFilter.includes(value))
+      newFilter = newFilter.filter((el) => el !== value)
+    else newFilter.push(value)
+    setSearchTags(newFilter)
+  }
 
   return loading ? (
     <Loading />
   ) : (
-    <div className="flex flex-col px-3">
-      <p>
-        List of {state} Events ({data?.length})
-      </p>
-      <div className="flex w-full justify-start">
-        {data.map((event) => (
-          <EventCard event={event} />
-        ))}
+    <div className="w-full px-2 ">
+      <div className="text-mono flex items-center overflow-auto">
+        <SearchIcon className="h-5 w-5 text-gray-800" />
+        {tags.map((tag) => {
+          const selected = searchTags.includes(tag)
+          return (
+            <div
+              key={tag}
+              className={`${
+                selected
+                  ? "bg-green-300 text-green-600"
+                  : "bg-gray-200 text-gray-600"
+              } group flex items-center space-x-2 w-max h-6 py-1 px-2 m-1 text-center cursor-pointer`}
+              onClick={() => filterTag(tag)}
+            >
+              <p className=" text-xs">{tag}</p>
+            </div>
+          )
+        })}
+      </div>
+      <div className="bg-white">
+        <div className="flex w-full justify-start overflow-auto">
+          {dataToDisplay.length ? (
+            dataToDisplay.map((event) => <EventCard event={event} />)
+          ) : (
+            <span className="font-medium flex space-x-4 items-center my-2 ml-2">
+              <EmojiSadIcon className="h-6 w-6" />
+              <p>Nothing to display</p>
+            </span>
+          )}
+        </div>
+        {data.length > perPage && (
+          <div className="border-b border-t mt-2 min-w-max w-full border-gray-200">
+            <div className="flex items-center justify-center">
+              <Pagination
+                setPage={setOffset}
+                currentPage={offset}
+                pageCount={pageCount}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
