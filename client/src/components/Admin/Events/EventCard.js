56ef6b1ffd449604fa9fb914aea140555a60ffb0
Victor Isidoro <victorisidoro@icloud.com>
2021-10-30 01:17:58 +0100

    EventCard

---
+++ client/src/components/Admin/Events/EventCard.js
@@ -0,0 +1,55 @@
+import { ChevronRightIcon, SpeakerphoneIcon } from "@heroicons/react/solid"
+import { Link } from "react-router-dom"
+import moment from "moment"
+
+const styles = {
+  online: "bg-flime-200 text-black border-flime-900 border p-1 px-2 text-xs",
+  public: "bg-fblue-100 text-white border-fblue-900 border p-1 px-2 text-xs",
+  private: "bg-fred-100 text-black border-fred-900 border p-1 px-2 text-xs",
+}
+
+const EventCard = ({ event }) => {
+  return (
+    <Link
+      className=" flex items-center justify-center my-3 mx-2"
+      to={`/admin/events/id/${event._id}`}
+    >
+      <div className="group cursor-pointer flex flex-col bg-white shadow-lg hover:shadow-xl">
+        <img
+          className="w-full h-24 bg-top bg-cover rounded-t"
+          src="https://www.si.com/.image/t_share/MTY4MTkyMjczODM4OTc0ODQ5/cfp-trophy-deitschjpg.jpg"
+          alt="cover"
+        />
+        <div className="w-full flex flex-col justify-between px-6 py-3">
+          <div className="w-full flex justify-between">
+            <p className="text-sm font-semibold text-red-400">
+              {moment(event.dateStart).format("DD/M/YYYY HH:mm")}
+            </p>
+            <p className={styles[event.type]}>{event.type}</p>
+          </div>
+
+          <p className="font-black text-mono text-lg tracking-wide w-80 truncate break-words">
+            {event.title}
+          </p>
+
+          <div className="w-80 my-2 text-sm text-gray-500 font-normal max-h-10 block overflow-ellipsis overflow-hidden break-words text-justify">
+            {event.description}
+          </div>
+
+          <div className="w-full flex items-center justify-between mt-2">
+            <div className="flex items-center">
+              <SpeakerphoneIcon className="w-4 h-4 text-gray-700 mr-2" />
+
+              <p className="text-gray-700 font-normal text-sm text-grotesk">
+                {event.organizer.firstName} {event.organizer.lastName}
+              </p>
+            </div>
+            <ChevronRightIcon className="w-6 h-6 group-hover:text-fblue" />
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default EventCard
--- client/src/components/Admin/Events/EventsList.js
+++ client/src/components/Admin/Events/EventsList.js
@@ -1,29 +1,69 @@
-import { useEffect, useState, useContext, useMemo } from "react"
+import { useEffect, useState, useContext } from "react"
 import axios from "axios"
 import AdminContext from "../../../contexts/Admin"
 import Loading from "../Widgets/Loading"
+import EventCard from "./EventCard"
 
 const eventsUrl = "/api/events/"
 
 const EventsList = ({ state }) => {
-  const [data, setData] = useState([])
-  const [loading, setLoading] = useState(true)
-  const { token, reload, selectedTab } = useContext(AdminContext)
-  const config = useMemo(() => {
-    return {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
+  const [data, setData] = useState([
+    {
+      _id: "1",
+      title: "Fundraiser",
+      photo: null,
+      description: "Fundraiser for Climate",
+      dateStart: "2021-11-02T10:00:00.000Z",
+      dateEnd: "2021-11-04T12:00:00.000Z",
+      address: "Geschwister-Scholl-Straße",
+      city: "Berlin",
+      geoLocation: {
+        lat: 52.5205888,
+        lng: 13.3912322,
       },
-    }
-  }, [token])
+      type: "public",
+      link: "",
+      tags: ["#fundraiser", "#climate-change"],
+      annouce: false,
+      zoom: 18,
+      location: "Library",
+      organizer: { _id: "1", firstName: "Victor", lastName: "Isidoro" },
+      interested: [],
+      going: [],
+    },
+    {
+      _id: "2",
+      title: "Conference",
+      photo: null,
+      description: "Conference on Food Shortage",
+      dateStart: "2021-11-02T10:00:00.000Z",
+      dateEnd: "2021-11-04T12:00:00.000Z",
+      address: "",
+      city: "",
+      geoLocation: {
+        lat: 52.5205888,
+        lng: 13.3912322,
+      },
+      type: "online",
+      link: "https://www.techonthenet.com/js/string_match.php",
+      tags: ["#food", "#speaker", "#online"],
+      annouce: false,
+      zoom: 18,
+      location: "",
+      organizer: { _id: "1", firstName: "Sasmitha", lastName: "Kumar" },
+      interested: [],
+      going: [],
+    },
+  ])
+  const [loading, setLoading] = useState(true)
+  const { token, reload, selectedTab, config } = useContext(AdminContext)
+  console.log(data)
 
   useEffect(() => {
     axios
       .get(eventsUrl + state, config)
       .then((res) => {
-        console.log(res)
-        setData(res.data.data)
+        if (res.data.data.length) setData(res.data.data)
         setLoading(false)
       })
       .catch((e) => {
@@ -34,8 +74,15 @@ const EventsList = ({ state }) => {
   return loading ? (
     <Loading />
   ) : (
-    <div>
-      List of {state} Events ({data?.length})
+    <div className="flex flex-col px-3">
+      <p>
+        List of {state} Events ({data?.length})
+      </p>
+      <div className="flex w-full justify-start">
+        {data.map((event) => (
+          <EventCard event={event} />
+        ))}
+      </div>
     </div>
   )
 }
