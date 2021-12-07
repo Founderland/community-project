28b0bbf7f268f967dff76b83746442dfc9dfb177
Salvo <salvopatti110@gmail.com>
2021-12-07 13:10:16 +0100

    homepage button

--- client/src/components/ApplicantsDispatcher.js
+++ client/src/components/ApplicantsDispatcher.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import { ReactComponent as FullLogo } from "../assets/full_logo.svg"
 import { ReactComponent as TwoLinesLogo } from "../assets/2_lines_logo.svg"
+import { ReactComponent as SmallLogo } from "../assets/small.svg"
 import FoundersLogo from "../assets/images/Founder.png"
 import InvestorsLogo from "../assets/images/Investor.gif"
 import AllyLogo from "../assets/images/Ally.png"
@@ -9,16 +10,21 @@ const ApplicantsDispatcher = () => {
   return (
     <>
       <div className=' h-screen md:h-full w-screen flex flex-col justify-between xl:items-center  '>
-        <div className='flex flex-col  justify-start items-center h-1/4 md:h-1/4 '>
-          <FullLogo className='hidden md:flex w-screen h-44 lg:h-60' />
-          <TwoLinesLogo className=' flex md:hidden h-auto w-screen' />
-          <h3 className='text-grotesk px-10 pb-5 text-xs md:text-base lg:text-xl xl:text-2xl'>
+        <div className='flex flex-col justify-center items-center h-1/4 md:h-1/4 mt-20 md:mt-0 '>
+          <a
+            href='https://www.founderland.org/'
+            className='absolute left-4 top-4 xl:top-6 xl:left-6 '>
+            <SmallLogo className=' w-12 h-12 lg:w-16 lg:h-16 p-1 text-white bg-black  transform duration-400 hover:bg-white hover:text-black' />
+          </a>
+          <FullLogo className='hidden md:flex w-screen h-44 lg:h-60 xl:w-screen' />
+          <TwoLinesLogo className=' flex md:hidden h-auto w-screen ' />
+          <h3 className='text-grotesk px-10 pb-5 xl:pb-10 text-xs md:text-base lg:text-xl xl:text-2xl'>
             WE’RE HERE TO BUILD A NEW INCLUSIVE, INTERSECTIONAL STANDARD FOR
             ENTREPRENEURS
           </h3>
         </div>
         <div className=' flex flex-col justify-evenly items-center h-3/4 w-screen md:h-1/4 xl:w-4/6 xl:flex-row '>
-          <div className='h-2/6 w-1/2  md:h-4/5 md:w-full xl:h-1/3 '>
+          <div className='h-2/6 w-4/5  md:h-4/5 md:w-full xl:h-1/3 '>
             <Link
               to='/form/founder'
               className=' h-full w-full flex bg-fred md:bg-white md:w-full justify-center items-center xl:flex-col-reverse '>
@@ -49,7 +55,7 @@ const ApplicantsDispatcher = () => {
             </Link>
           </div>
 
-          <div className='h-2/6 w-1/2 my-3 md:m-0 md:h-4/5 md:w-full '>
+          <div className='h-2/6 w-4/5 my-3 md:m-0 md:h-4/5 md:w-full '>
             <Link
               to='/form/investor'
               className=' h-full w-full flex bg-fblue md:bg-white md:w-full justify-center items-center md:flex-row-reverse xl:flex-col-reverse'>
@@ -79,7 +85,7 @@ const ApplicantsDispatcher = () => {
               </div>
             </Link>
           </div>
-          <div className='h-2/6 w-1/2  md:h-4/5 md:w-full '>
+          <div className='h-2/6 w-4/5  md:h-4/5 md:w-full '>
             <Link
               to='/form/ally'
               className=' h-full w-full flex bg-flime md:bg-white md:w-full justify-center items-center xl:flex-col-reverse'>
