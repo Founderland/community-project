16df57e9151e699be08fbea79ca266734f9c1c21
Victor Isidoro <victorisidoro@icloud.com>
2021-12-15 21:33:54 +0000

    Removed headline from Forms

--- client/src/components/Forms/FormPage.js
+++ client/src/components/Forms/FormPage.js
@@ -66,9 +66,9 @@ const FormPage = (props) => {
   return (
     <div>
       {gify && (
-        <div classname='absolute w-screen h-screen pb-10'>
-          <img src={gif} alt='gif' />
-          <p className='text-4xl font-monserrat font-semibold m-10'>
+        <div classname="absolute w-screen h-screen pb-10">
+          <img src={gif} alt="gif" />
+          <p className="text-4xl font-monserrat font-semibold m-10">
             You Form is getting Submitted !!
           </p>
         </div>
@@ -80,20 +80,15 @@ const FormPage = (props) => {
             !gify
               ? "max-h-screen w-screen md:w-full flex justify-center flex-col overflow-hidden"
               : "hidden"
-          }>
-          <div className=' flex flex-col justify-between p-10 h-screen overflow-hidden'>
-            <div className='flex  justify-between md:justify-end mb-7'>
-              <div className='md:hidden mt-3'>
-                <img src={smallLogo} alt='logo' className='w-20 pr-5' />
-              </div>
-              <div className=' '>
-                <h2 className=' hidden md:flex md:mt-5 text-mono font-semibold md:text-xl lg:text-xl xl:text-2xl md:tracking-wider uppercase'>
-                  DISRUPTING THE PIPELINE FOR{" "}
-                  {memberType === "ally" ? "allies" : memberType + "s"}
-                </h2>
+          }
+        >
+          <div className=" flex flex-col justify-between p-10 h-screen overflow-hidden">
+            <div className="flex  justify-between md:justify-end mb-7">
+              <div className="md:hidden mt-3">
+                <img src={smallLogo} alt="logo" className="w-20 pr-5" />
               </div>
             </div>
-            <div className='w-full h-full overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-blue-100 scrollbar-thumb-fblue pb-10'>
+            <div className="w-full h-full overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-blue-100 scrollbar-thumb-fblue pb-10">
               {questions.map((question, i) => (
                 <Question
                   key={i}
@@ -104,17 +99,18 @@ const FormPage = (props) => {
                 />
               ))}
             </div>
-            <div className='px-5 py-4 mb:py-8 '>
+            <div className="px-5 py-4 mb:py-8 ">
               {!isFirst && (
                 <button
-                  type='button'
-                  className='z-10 py-2 px-4 xl:py-4 float-left bg-fblue opacity-80 hover:bg-fblue-dark focus:ring-fblue focus:ring-offset-white text-white text-mono w-2/5 md:w-1/3 xl:w-1/4 transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 '
-                  onClick={previous}>
+                  type="button"
+                  className="z-10 py-2 px-4 xl:py-4 float-left bg-fblue opacity-80 hover:bg-fblue-dark focus:ring-fblue focus:ring-offset-white text-white text-mono w-2/5 md:w-1/3 xl:w-1/4 transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 "
+                  onClick={previous}
+                >
                   Prev
                 </button>
               )}
               <button
-                type='submit'
+                type="submit"
                 disabled={isLast && props.questionPreview}
                 className={
                   "py-2 px-4  xl:py-4 float-right focus:ring-offset-white text-white text-mono w-2/5 md:w-1/3 xl:w-1/4 transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 " +
@@ -123,7 +119,8 @@ const FormPage = (props) => {
                     : "bg-fblue hover:bg-fblue-dark focus:ring-fblue") +
                   (isLast && props.questionPreview && " opacity-30")
                 }
-                onClick={nextClicked}>
+                onClick={nextClicked}
+              >
                 {isLast ? "Submit" : "next"}
               </button>
             </div>
